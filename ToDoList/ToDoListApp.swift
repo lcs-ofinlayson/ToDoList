28adaedb60408bb314cca549e060b85da09a59cf
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-04-04 09:14:21 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Oliver Finlayson on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
