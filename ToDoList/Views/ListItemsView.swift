465a2af7172dd80bd91ed57c148debb9325203c2
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-04-17 23:26:11 -0400

    Almost finished separating ListView and ListItemsView to allow search to work. About to add the initializer function.

---
+++ ToDoList/Views/ListItemsView.swift
@@ -0,0 +1,91 @@
+//
+//  ListView.swift
+//  ToDoList
+//
+//  Created by Oliver Finlayson on 2023-04-04.
+//
+
+import Blackbird
+import SwiftUI
+
+struct ListItemsView: View {
+    
+    //MARK: Stored Properties
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
+    
+    @BlackbirdLiveModels({ db in
+        try await TodoItem.read(from: db,
+        sqlWhere: "description LIKE ?", "%\(searchText)%")
+        
+    }) var todoItems
+    
+    //MARK: Computed Properties
+    var body: some View {
+        List{
+            
+            ForEach(todoItems.results) { currentItem in
+                
+                
+                
+                
+                
+                
+                Label(title: {
+                    Text (currentItem.description)
+                }, icon: {
+                    if currentItem.completed == true {
+                        Image (systemName: "checkmark.circle")
+                    } else {
+                        Image (systemName: "circle")
+                    }
+                })
+                
+                .onTapGesture {
+                    Task{
+                        try await db!.transaction { core in
+                            
+                            try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)",
+                                           !currentItem.completed,
+                                           currentItem.id)
+                        }
+                    }
+                }
+            }
+            .onDelete(perform: removeRows)
+            
+        }
+    }
+    
+    //MARK: Functions
+    func removeRows(at offsets: IndexSet) {
+        
+        Task{
+            
+            try await db!.transaction { core in
+                
+                var idList = ""
+                for offset in offsets{
+                    idList += "\(todoItems.results[offset].id),"
+                }
+                
+                print(idList)
+                idList.removeLast()
+                print(idList)
+                
+                
+                try core.query("DELETE FROM TodoItem WHERE id IN (?)", idList)
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+struct ListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListItemsView()
+    }
+}
